11734623679644cefd7f492514e4f1df2e938404
Sal-1807 <salonipalo735@gmail.com>
2026-02-23 14:48:22 +0530

    UC2: Hardcoded palindrome

--- src/Palindrome/PalindromeChecker.java
+++ src/Palindrome/PalindromeChecker.java
@@ -2,6 +2,15 @@ package Palindrome;
 
 public class PalindromeChecker {
     public static void main(String[] args) {
+
         System.out.println("Welcome to Palindrome Checker App");
+
+        String word = "madam";
+
+        if(word.equals(new StringBuilder(word).reverse().toString())) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
     }
 }
