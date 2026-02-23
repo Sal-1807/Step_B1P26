1cc7841198a8aa14300f3c0a3e7af1ae3ee4a036
Saloni Palo <salonipalo735@gmail.com>
2026-02-23 15:52:53 +0530

    hehe
    Removed palindrome checking logic and updated greeting.

--- src/Hello/HelloWorld.java
+++ src/Hello/HelloWorld.java
@@ -3,14 +3,6 @@ package Hello;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("Hello");
 
-        String word = "madam";
-
-        if(word.equals(new StringBuilder(word).reverse().toString())) {
-            System.out.println(word + " is a Palindrome");
-        } else {
-            System.out.println(word + " is NOT a Palindrome");
-        }
-    }
 }
