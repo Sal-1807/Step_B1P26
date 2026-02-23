a1cfa6595f698d01a1f9d9003013c6b87873f5c1
Sal-1807 <salonipalo735@gmail.com>
2026-02-23 14:45:53 +0530

    UC2: Hardcoded palindrome

--- src/Hello/HelloWorld.java
+++ src/Hello/HelloWorld.java
@@ -1,7 +1,16 @@
 package Hello;
 
 public class HelloWorld {
-    static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Palindrome Checker App");
+
+        String word = "madam";
+
+        if(word.equals(new StringBuilder(word).reverse().toString())) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
     }
 }
